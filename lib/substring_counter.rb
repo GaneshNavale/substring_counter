bbc5302ea6ad62a4d885a3ce93f02d412969ee0b
Ganesh Navale <navaleganesh2011@gmail.com>
2019-06-06 23:23:12 +0530

    Update substring_counter.rb

--- lib/substring_counter.rb
+++ lib/substring_counter.rb
@@ -7,6 +7,7 @@ module SubstringCounter
 		unless args.all? { |arg| arg.instance_of? String }
 			raise ArgumentError, "Must enter String arguments"
 		end
+		
 		result = {}
 		args.each { |keyword| result[keyword] = self.scan(keyword).count }
 		args.length.eql?(1) ? result[args.first] : result
